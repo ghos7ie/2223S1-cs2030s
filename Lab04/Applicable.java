0e87f2d661a772187e04fe00fae343568ad185b8
ghos7ie <elyreum7@gmail.com>
2022-09-27 22:41:44 +0800

    comments

--- Lab04/Applicable.java
+++ Lab04/Applicable.java
@@ -14,10 +14,15 @@
 
 interface Applicable<T> {
   /**
+   * Takes in {@code Probably<Immutator>} and mutates value inside
+   * {@code Probably<Immutator>}.
    * 
-   * @param <R>
-   * @param immutator
-   * @return
+   * @param <R>      Explicit type parameter. Telling compiler that the type of
+   *                 return will be R.
+   * @param probably {@code Probably<Immutator>} to be mutated.
+   * @return {@code Probably<R>} if immutator is this.value is not null and is an
+   *         immutator.
+   *         Else none();
    */
   <R> Probably<R> apply(Probably<? extends Immutator<? extends R, ? super T>> probably);
 }
