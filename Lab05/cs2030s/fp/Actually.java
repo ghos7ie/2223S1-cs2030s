742d1d6a0a72a6014b104233dc91be4dbe58ce96
ghos7ie <elyreum7@gmail.com>
2022-10-06 12:32:26 +0800

    format

--- Lab05/cs2030s/fp/Actually.java
+++ Lab05/cs2030s/fp/Actually.java
@@ -1,72 +1,74 @@
 package cs2030s.fp;
 
-/** 
+/**
  * @version CS2030S Lab 4
  *          AY22/23 Semester 1
  *
  * @author Lewis Lye [14A]
  */
 
-public abstract class Actually<T>{
+public abstract class Actually<T> {
 
   /**
-   *  ok(res) returns a Success Object.
-   *  @param res variable of type T to put into Success.
+   * ok(res) returns a Success Object.
+   * 
+   * @param res
+   *          variable of type T to put into Success.
    *
-   *  @return new Success().
+   * @return new Success().
    *
-   */ 
-  public static <T> Success<T> ok (T res){
-    return new Success<> (res);
+   */
+  public static <T> Success<T> ok(T res) {
+    return new Success<>(res);
   }
 
   /**
    * err(exception) returns a Failure Object.
    * 
-   * @param exception object of type exception.
+   * @param exception
+   *          object of type exception.
    *
    * @return new Failure().
    */
-  public static <T> Failure err(Exception exception){
+  public static <T> Failure err(Exception exception) {
     return new Failure(exception);
   }
 
   static final class Success<T> extends Actually<T> {
-     
+
     private final T value;
-    
+
     /**
      * Constuctor for Success class.
      *
-     * @param val value of type T.
+     * @param val
+     *          value of type T.
      *
      */
-    public Success (T val){
-       this.value = val;
-     }
+    public Success(T val) {
+      this.value = val;
+    }
 
     /**
      * Equals implementation for Success class.
      *
-     * @param object type of Object.
+     * @param object
+     *          type of Object.
      *
      * @return true if value of object is equal to value of Success.
      *         false if not equal.
-     */ 
+     */
     @Override
-    public boolean equals (Object obj){
-      if (obj instanceof Success){
-        if ((Success) obj.value == null){
+    public boolean equals(Object obj) {
+      if (obj instanceof Success) {
+        if ((Success) obj.value == null) {
           return false;
-        }
-        else if ((Success) obj.value == this.value){
+        } else if ((Success) obj.value == this.value) {
           return true;
-        }
-        else{
+        } else {
           return false;
         }
-      }
-      else{
+      } else {
         return false;
       }
     }
@@ -77,63 +79,61 @@ public abstract class Actually<T>{
      * @return {@code <null>} if this.value is null.
      *         value.toString() if not null.
      */
-     @Override
-     public String toString(){
-       if (this.value != null){
-         return "<" + this.value.toString() + ">";
-       } 
-       else{
-       return "<null>";
-       }
-     }
-   }
+    @Override
+    public String toString() {
+      if (this.value != null) {
+        return "<" + this.value.toString() + ">";
+      } else {
+        return "<null>";
+      }
+    }
+  }
 
-   static final class Failure extends Actually<Object>{
-     
-     private final Exception obj;
-     
-     /**
-      * Constructor for Failure class.
-      *
-      * @param obj obj of type Exception.
-      *
-      */ 
-     public Failure (Exception obj){
-       this.obj = obj;
-     }
+  static final class Failure extends Actually<Object> {
 
-      
-    /** 
+    private final Exception obj;
+
+    /**
+     * Constructor for Failure class.
+     *
+     * @param obj
+     *          obj of type Exception.
+     *
+     */
+    public Failure(Exception obj) {
+      this.obj = obj;
+    }
+
+    /**
      * Equals implementation for Failure class.
      *
-     * @param object type of Object.
+     * @param object
+     *          type of Object.
      *
      * @return true if getMessage() of object is equal to that of Failure.
      *         false if not equal.
-     */ 
+     */
     @Override
-    public boolean equals (Object obj){
-      if (obj instanceof Failure){
-        if (obj.getMessage() == this.obj.getMessage()){
+    public boolean equals(Object obj) {
+      if (obj instanceof Failure) {
+        if (obj.getMessage() == this.obj.getMessage()) {
           return true;
-        }
-        else{
+        } else {
           return false;
         }
-      }
-      else{
+      } else {
         return false;
       }
     }
 
-     /**
-      * Returns String representation of Failure object.
-      *
-      * @return value in formatted manner.
-      */ 
-     @Override
-     public String toString(){
-       return "[" + this.obj.getClass().toString() + "] " + this.obj.getMessage();
-     }
+    /**
+     * Returns String representation of Failure object.
+     *
+     * @return value in formatted manner.
+     */
+    @Override
+    public String toString() {
+      return "[" + this.obj.getClass().toString() + "] " + this.obj.getMessage();
+    }
   }
 }
