2adccee456dfd5167abc6fae61d4d1badbd4d2d7
LYE CHENG SEE LEWIS <lewis7@comp.nus.edu.sg>
2022-10-06 12:18:05 +0800

    tetisng

--- Lab05/cs2030s/fp/Actually.java
+++ Lab05/cs2030s/fp/Actually.java
@@ -9,41 +9,131 @@ package cs2030s.fp;
 
 public abstract class Actually<T>{
 
-  private final T value;
-
+  /**
+   *  ok(res) returns a Success Object.
+   *  @param res variable of type T to put into Success.
+   *
+   *  @return new Success().
+   *
+   */ 
   public static <T> Success<T> ok (T res){
     return new Success<> (res);
   }
 
+  /**
+   * err(exception) returns a Failure Object.
+   * 
+   * @param exception object of type exception.
+   *
+   * @return new Failure().
+   */
   public static <T> Failure err(Exception exception){
     return new Failure(exception);
   }
 
-   static final class Success<T> extends Actually<T> {
+  static final class Success<T> extends Actually<T> {
      
-     private final T value;
-
-     public Success<T> (value){
-       this.value = value;
+    private final T value;
+    
+    /**
+     * Constuctor for Success class.
+     *
+     * @param val value of type T.
+     *
+     */
+    public Success (T val){
+       this.value = val;
      }
 
+    /**
+     * Equals implementation for Success class.
+     *
+     * @param object type of Object.
+     *
+     * @return true if value of object is equal to value of Success.
+     *         false if not equal.
+     */ 
+    @Override
+    public boolean equals (Object obj){
+      if (obj instanceof Success){
+        if ((Success) obj.value == null){
+          return false;
+        }
+        else if ((Success) obj.value == this.value){
+          return true;
+        }
+        else{
+          return false;
+        }
+      }
+      else{
+        return false;
+      }
+    }
+
+    /**
+     * Returns String representation of Success object.
+     *
+     * @return {@code <null>} if this.value is null.
+     *         value.toString() if not null.
+     */
      @Override
      public String toString(){
-       return "<" + this.value.toString() + ">";
+       if (this.value != null){
+         return "<" + this.value.toString() + ">";
+       } 
+       else{
+       return "<null>";
+       }
      }
    }
 
    static final class Failure extends Actually<Object>{
-      
-     private final Object obj;
      
-     public Failure (Object obj){
+     private final Exception obj;
+     
+     /**
+      * Constructor for Failure class.
+      *
+      * @param obj obj of type Exception.
+      *
+      */ 
+     public Failure (Exception obj){
        this.obj = obj;
      }
 
+      
+    /** 
+     * Equals implementation for Failure class.
+     *
+     * @param object type of Object.
+     *
+     * @return true if getMessage() of object is equal to that of Failure.
+     *         false if not equal.
+     */ 
+    @Override
+    public boolean equals (Object obj){
+      if (obj instanceof Failure){
+        if (obj.getMessage() == this.obj.getMessage()){
+          return true;
+        }
+        else{
+          return false;
+        }
+      }
+      else{
+        return false;
+      }
+    }
+
+     /**
+      * Returns String representation of Failure object.
+      *
+      * @return value in formatted manner.
+      */ 
      @Override
      public String toString(){
-       return "[" + this.obj.getClass() + "] " + this.obj.getMessage();
+       return "[" + this.obj.getClass().toString() + "] " + this.obj.getMessage();
      }
   }
 }
