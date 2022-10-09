eeb113c85b2b5df4d349cd6d3379348cc78af67a
ghos7ie <elyreum7@gmail.com>
2022-10-09 17:34:35 +0800

    nice typo

--- Lab05/Test1.java
+++ Lab05/Test1.java
@@ -57,7 +57,7 @@ public class Test1 {
     we.expect(
       "Actually.ok(null).equals(Actually.ok(null))",
       cs2030s.fp.Actually.ok(null).equals(cs2030s.fp.Actually.ok(null)),
-      true
+      false
     );
     we.expect(
       "Actually.ok(null).equals(\"Err\")",
