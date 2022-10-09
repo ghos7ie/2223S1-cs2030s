e7613b6d8d67c805b19ba83248a1bd22da1ccc49
ghos7ie <elyreum7@gmail.com>
2022-10-09 22:35:57 +0800

    WHO THE FUCK KNOWS THIS SHIT

--- Lab05/cs2030s/fp/Actually.java
+++ Lab05/cs2030s/fp/Actually.java
@@ -284,7 +284,7 @@ public abstract class Actually<T> implements Immutatorable<T> {
      */
     @Override
     public String toString() {
-      return "[" + this.exception.toString() + "] " + this.exception.getMessage();
+      return "[" + this.exception.getClass().getCanonicalName()+ "] " + this.exception.getMessage();
     }
   }
 }
