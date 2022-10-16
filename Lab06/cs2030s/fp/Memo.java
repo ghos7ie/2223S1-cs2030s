08f3087e147a07b55ca11529ac61d03c2e318c2b
ghos7ie <elyreum7@gmail.com>
2022-10-16 22:27:07 +0800

    Update Memo.java

--- Lab06/cs2030s/fp/Memo.java
+++ Lab06/cs2030s/fp/Memo.java
@@ -1,5 +1,5 @@
-package cs2030s.fp;
+// package cs2030s.fp;
 
-public class Memo<T> extends Lazy<T> {
-  private Actually<T> value;
-}
+// public class Memo<T> extends Lazy<T> {
+//   private Actually<T> value;
+// }
