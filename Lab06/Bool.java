b59cd24efb01f99ca9061ac4b89b61cf51bca137
ghos7ie <elyreum7@gmail.com>
2022-10-18 13:57:28 +0800

    Update Bool.java

--- Lab06/Bool.java
+++ Lab06/Bool.java
@@ -15,7 +15,7 @@ class Bool implements Cond {
 
   @Override
   public String toString() {
-    return this.val.transform(b -> b.toString()).transform(s -> s.substring(0, 1)).toString();
+    return this.val.transform(s -> s.toString().substring(0, 1)).toString();
   }
 
   @Override
