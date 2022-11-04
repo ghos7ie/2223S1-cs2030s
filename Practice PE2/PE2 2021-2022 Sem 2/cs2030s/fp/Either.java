32501ce887b8ace409ef9af0e9313e6d8d739efc
LYE CHENG SEE LEWIS <lewis7@comp.nus.edu.sg>
2022-11-04 18:45:46 +0800

    me do on vscode

--- Practice PE2/PE2 2021-2022 Sem 2/cs2030s/fp/Either.java
+++ Practice PE2/PE2 2021-2022 Sem 2/cs2030s/fp/Either.java
@@ -4,3 +4,89 @@
  *
  * @author A0000000X
  */
+
+package cs2030s.fp;
+
+import java.util.NoSuchElementException;
+
+abstract class Either<L,R>{
+  protected L leftVal;
+  protected R rightVal;
+
+  protected Either(L value){
+    this.leftVal = value;
+    this.rightVal = null;
+  }
+
+  protected Either(R value){
+    this.leftVal = null;
+    this.rightVal = value;
+  }
+
+  // abstract mehtods
+  public abstract boolean isLeft();
+  public abstract boolean isRight();
+  public abstract L getLeft();
+  public abstract R getRight();
+
+  public static <L,R> Either<L,R> left(L value){
+    return new Left(value);
+  }
+
+  public static <L,R> Either<L,R> right(R value){
+    return new Right(value);
+  }
+
+  private static class Left<L,R> extends Either<L,R>{
+    protected Left(L val){
+      super(val);
+    }
+
+    @Override
+    public boolean isLeft(){
+      return true;
+    }
+    
+    @Override
+    public boolean isRight(){
+      return false;
+    }
+
+    @Override
+    public L getLeft(){
+      return this.leftValue;
+    }
+
+    @Override
+    public R getRight(){
+      throw new NoSuchElementException();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+      if (obj == this){
+        return true;
+      }
+      if (obj instanceof Left){
+        return true;
+      }
+      if (obj instanceof Right){
+        return this.leftValue == obj.rightValue;  
+      }
+      return false;
+    }
+
+    @Override
+    public String toString(){
+      return "Left[" + this.leftValue.toString() + "]";
+    }
+  
+  }
+
+  private static class Right<L,R> extends Either<L,R>{
+    protected Right(R val){
+      super(val);
+    }
+  }
+
+}
