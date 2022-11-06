811393b1f6f140be01f9625b147b2ed9c19463cd
ghos7ie <elyreum7@gmail.com>
2022-11-06 12:27:35 +0800

    ?

--- Lab08/cs2030s/fp/InfiniteList.java
+++ Lab08/cs2030s/fp/InfiniteList.java
@@ -175,12 +175,12 @@ public class InfiniteList<T> {
         // uses check(pred)
         // if head value fails (because err or fails check), return err
         // else returns the Actually<head>.
-        Memo.from(() -> this.head.get().check(x -> isEnd()).check(pred)),
+        this.head,
         // check if head passes the test
         // if it passes recrusively call takeWhile on tail
         // else return end
         Memo.from(
-            () -> this.head.get().check(x -> isEnd()).check(pred).transform(t -> this.tail.get().takeWhile(pred)).unless(end())));
+            () -> this.head.get().check(pred).transform(t -> this.tail.get().takeWhile(pred)).except(() -> end())));
   }
 
   /**
