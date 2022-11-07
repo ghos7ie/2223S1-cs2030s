7b98f4f660fa17979d1b60cc67bc47f2580171ba
ghos7ie <elyreum7@gmail.com>
2022-11-07 17:45:12 +0800

    Update Lab8.h

--- Lab08/Lab8.h
+++ Lab08/Lab8.h
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 mkdir cs2030s
 cd cs2030s
 mkdir fp
 cd ..
-
 mv Action.java cs2030s/fp/Action.java
 mv Actionable.java cs2030s/fp/Actionable.java
 mv Actually.java cs2030s/fp/Actually.java
@@ -13,7 +11,6 @@ mv Constant.java cs2030s/fp/Constant.java
 mv Immutator.java cs2030s/fp/Immutator.java
 mv Immutatorable.java cs2030s/fp/Immutatorable.java
 mv Memo.java cs2030s/fp/Memo.java
-mv Memo.java cs2030s/fp/InfiniteList.java
-
+mv InfiniteList.java cs2030s/fp/InfiniteList.java
 javac cs2030s/fp/*.java
 javac *.java
