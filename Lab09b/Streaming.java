e5ad472da3a878d15202afc9c3a7ec43961fd905
ghos7ie <elyreum7@gmail.com>
2022-11-11 08:33:32 +0800

    Update Streaming.java

--- Lab09b/Streaming.java
+++ Lab09b/Streaming.java
@@ -6,22 +6,24 @@ import java.util.LinkedList;
 public class Streaming {
   public static <T> List<Pair<Integer, T>> encode(Stream<T> stream) {
     return stream
-        .collect(
-            // Supplier - The ArrayList to add to
-            LinkedList<Pair<Integer, T>>::new,
-            (list, value) -> {
-              if (list.isEmpty() || !list.getLast().getSnd().equals(value)) {
-                list.add(new Pair<Integer, T>(1, value));
-              }
-              list.getLast().setFst(list.getLast().getFst() + 1);
-            },
-            (list1, list2) -> {
-              if (list1.getLast().getSnd().equals(list2.getFirst().getSnd())) {
-                list1.getLast().setFst(list1.getLast().getFst() + list2.getFirst().getFst());
-                list2.removeFirst();
-              }
-              list1.addAll(list2);
-            })
-        .stream().collect(Collectors.toList());
+        .map(value -> new Pair<Integer, T> (1, value))
+        .collect(LinkedList<Pair<Integer, T>>::new, 
+        (list, pair) -> {
+          // if list is empty OR the last pair does not have the same T value
+          // add new Pair into the list
+          if (list.isEmpty() || !list.getLast().getSnd().equals(pair.getSnd())){
+            list.add(pair);
+          }else{
+            // else since same value consecutively, we add 1 to count
+            list.getLast().setFst(list.getLast().getFst() + 1);
+          }
+        }, 
+        (list1, list2) -> {
+          if (list1.getLast().getSnd().equals(list2.getFirst().getSnd())){
+            list1.getLast().setFst(list1.getLast().getFst() + list2.getFirst().getFst());
+            list2.removeFirst();
+          }
+          list1.addAll(list2);
+        });
   }
 }
